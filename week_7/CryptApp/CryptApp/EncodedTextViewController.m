50e0f0372692a30f91ab1c569147779d86e11ad8
Drue Thomas <Julie@drues-macbook-pro.local>
2015-10-16 21:03:36 -0400

    fixed outlets

--- week_7/CryptApp/CryptApp/EncodedTextViewController.m
+++ week_7/CryptApp/CryptApp/EncodedTextViewController.m
@@ -19,7 +19,7 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     
-    self.encodedTextLabel.text = self.text.text;
+    self.encodedTextLabel.text = self.text;
 }
 
 - (void)didReceiveMemoryWarning {
